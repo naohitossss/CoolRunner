4801b2495e61c62a59d751d7b87f9fec336efe78
naohito <xieshanrge@gmail.com>
2025-01-07 18:22:38 +0900

    制作途中

--- Assets/script/MapGenerastion/RandomMapGena.cs
+++ Assets/script/MapGenerastion/RandomMapGena.cs
@@ -2,13 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
+
+public struct Bliuding 
+{
+    int num;
+    GameObject bliudingObject;
+}
 
 public class RandomMapGena : MonoBehaviour
 {
+    [SerializeField] private List<Bliuding> bliudings = new List<Bliuding>();
+    [SerializeField] private List<GameObject> obstacles = new List<GameObject>();
+    [SerializeField] private GameObject streatObj;
+    [SerializeField] private GameObject grassObj;
+
+
     private List<List<int>> mapList =  new List<List<int>>();
     private int buildMapNum = 0;
     private int streatCount = 3;
     private int startMapRow = 24;
+    private float streatObjLegth;
+    private float streatObjTall;
+    private Vector3 setStreatPos;
+    private Vector3 setGrassPos;
 
     void Awake()
     {
@@ -18,7 +35,9 @@ public class RandomMapGena : MonoBehaviour
 
     void Start()
     {
-        
+        setStreatPos = Vector3.zero + new Vector3(0f, 0f, streatObjLegth / 2);
+        setGrassPos = Vector3.zero + new Vector3(0f,
+
     }
 
     // Update is called once per frame
@@ -31,30 +50,17 @@ public class RandomMapGena : MonoBehaviour
     {
         for(int row = 0;row < startMapRow;row++)
         {
-            for(int col = 0;col < streatCount;col++)
+            Instantiate(streatObj, setStreatPos, Quaternion.identity);
+            setStreatPos += new Vector3(0f,0f,streatObjLegth / 2);
+
+
+            for (int col = 0;col < streatCount;col++)
             {
                 List<int> mapRow = new List<int>();
-                mapRow.Add(Random.Range(0,3));
-                switch (mapRow[0])
+                mapRow.Add(Random.Range(1,4));
+                for(int i = mapRow[0]; i <= streatCount;i++)
                 {
-                    case 0:
-                        for(int i = 1;i <= streatCount;i++)
-                        {
-                            mapRow[i] = 0;
-                        }
-                        break;
-                    case 1:
-                        for(int i = 2;i <= streatCount;i++)
-                        {
-                            mapRow[i] = 0;
-                        }
-                        break;
-                    case 2:
-                        for(int i = 3;i <= streatCount;i++)
-                        {
-                            mapRow[i] = 0;
-                        }
-                        break;
+                    mapRow[i] = 0;
                 }
                 mapList.Add(mapRow);    
             }
